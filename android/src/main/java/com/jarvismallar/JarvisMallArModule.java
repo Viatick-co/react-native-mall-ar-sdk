f8f9e54bcf0075d616df40fb84dccb251fb67b46
Hieu Nguyen <jack@viatick.com>
2022-12-21 11:10:50 +0700

    chore: initial commit

---
+++ android/src/main/java/com/jarvismallar/JarvisMallArModule.java
@@ -0,0 +1,32 @@
+package com.jarvismallar;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = JarvisMallArModule.NAME)
+public class JarvisMallArModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "JarvisMallAr";
+
+  public JarvisMallArModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
